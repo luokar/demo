6a0a34416811aeb7eb195d4cebed5429e99d4375
luokar <luokar@gmail.com>
2018-12-20 00:09:05 +0800

    commit

--- src/main/java/com/example/demo/Swagger2Config.java
+++ src/main/java/com/example/demo/Swagger2Config.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import io.vavr.jackson.datatype.VavrModule;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.configuration.ObjectMapperConfigured;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,14 +16,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @Import(BeanValidatorPluginsConfiguration.class)
-public class Swagger2Config {
+public class Swagger2Config implements ApplicationListener<ObjectMapperConfigured> {
 
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
-                .paths(PathSelectors.ant("/v1/**"))
+                .paths(PathSelectors.ant("/v2/**"))
                 .build();
+    }
+
+
+    @Override
+    public void onApplicationEvent(ObjectMapperConfigured event) {
+        event.getObjectMapper().registerModule(new VavrModule());
     }
 }
--- src/main/java/com/example/demo/interfaces/DummyController.java
+++ src/main/java/com/example/demo/interfaces/DummyController.java
@@ -1,12 +1,14 @@
 package com.example.demo.interfaces;
 
 import io.vavr.collection.List;
+import io.vavr.collection.Set;
+import io.vavr.collection.TreeSet;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/persons/")
+@RequestMapping("/v2/persons/")
 public class DummyController {
 
     @GetMapping("abc")
@@ -15,4 +17,8 @@ public class DummyController {
         return List.of("a", "b", "c");
     }
 
+    @GetMapping("tuple")
+    public List<Set<String>> getTuple() {
+        return List.of(TreeSet.of("a", "c"));
+    }
 }
